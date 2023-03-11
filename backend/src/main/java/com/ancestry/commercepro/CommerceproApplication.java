18ae2d257ab000e163bd7fcd8c385c644cd0add3
deepaksama <deepak.sama@gmail.com>
2023-03-11 01:35:10 -0700

    Resolved merge conflicts

---
+++ backend/src/main/java/com/ancestry/commercepro/CommerceproApplication.java
@@ -0,0 +1,13 @@
+package com.ancestry.commercepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceproApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ancestry/commercepro/CommerceproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ancestry.commercepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
